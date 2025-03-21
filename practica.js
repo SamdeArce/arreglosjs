1010f0b67af44f6f87bdb0d7858c070eec297672
SamdeArce <samanthalizettquintoramirez@gmail.com>
2025-03-20 22:47:12 -0600

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,54 @@
+/*
+let numero = [10, 20, 30, 40]
+console.log(numero)
+*/
+/*
+let planetas = ["Marte", "Jupiter", "saturno"]
+console.log(planetas)
+
+//acceder a los elementos de  un arreglo
+console.log("Primer planeta: ", planetas[1]);
+console.log("Ultimo  planeta: ", planetas[planetas.length - 1]);
+*/
+
+/*
+//metodos comunes de los arreglos 
+let planetas = ["Venus", "Tierra", "Marte", "Jupiter"]
+
+//agregar elementos
+planetas.push("saturno")//agregar al final
+console.log("Despues de push: ", planetas)
+
+//eliminar elementos
+planetas.pop() //elimina el ultimo
+console.log("Despues de pop:", planetas)
+
+planetas.unshift("Mercurio") //agrega al inicio
+console.log("Despues de unshift: ", planetas)
+
+planetas.shift() //elimina el primero
+console.log("Despues de shift: ", planetas)
+
+//propiedades utiles de los arreglos
+console.log("Numero de planetas: ", planetas.length)
+console.log("¿El arreglo incliye a 'Venus'?", planetas.includes("Venus"))*/
+
+/*
+//ordenar un arreglo
+let numeros = [3, 4, 1, 5, 7, 2, 6]
+numeros.sort(); //ordena de menor a mayor
+console.log("Ordenados: ", numeros)
+
+numeros.reverse(); //invierte el orden
+console.log("Invertidos: ", numeros)*/
+
+/*
+//arreglos multidimencionales
+let matriz =[
+    ["juan pablo", 18],
+    ["valeria", 20],
+    ["Alan", 19]
+]
+
+console.log("Edad de pablo: ", matriz[1][1])
+*/
